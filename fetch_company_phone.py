ce2365c8821a4cec1cde3191165011dfbd83c2ae
Ogpavan <pawantwp@gmail.com>
2026-03-01 22:16:46 +0530

    Make Vercel function self-contained and remove curl subprocess

--- fetch_company_phone.py
+++ fetch_company_phone.py
@@ -1,8 +1,8 @@
 import argparse
 import re
-import subprocess
 import sys
 from urllib.parse import quote, unquote
+from urllib.request import Request, urlopen
 
 BASE_URL = "https://www.google.com/search?tbm=map&authuser=0&hl=en&gl=in&pb=!4m12!1m3!1d53758.17799235329!2d79.43663169999999!3d28.366764999999997!2m3!1f0!2f0!3f0!3m2!1i870!2i735!4f13.1!7i20!10b1!12m26!1m5!18b1!30b1!31m1!1b1!34e1!2m4!5m1!6e2!20e3!39b1!10b1!12b1!13b1!16b1!17m1!3e1!20m4!5e2!6b1!8b1!14b1!46m1!1b0!96b1!99b1!19m4!2m3!1i360!2i120!4i8!20m65!2m2!1i203!2i100!3m2!2i4!5b1!6m6!1m2!1i86!2i86!1m2!1i408!2i240!7m33!1m3!1e1!2b0!3e3!1m3!1e2!2b1!3e2!1m3!1e2!2b0!3e3!1m3!1e8!2b0!3e3!1m3!1e10!2b0!3e3!1m3!1e10!2b1!3e2!1m3!1e10!2b0!3e4!1m3!1e9!2b1!3e2!2b1!9b0!15m16!1m7!1m2!1m1!1e2!2m2!1i195!2i195!3i20!1m7!1m2!1m1!1e2!2m2!1i195!2i195!3i20!22m6!1siWekaenqNYOX4-EPh5fb-Qc%3A4!2s1i%3A0%2Ct%3A11887%2Cp%3AiWekaenqNYOX4-EPh5fb-Qc%3A4!7e81!12e3!17siWekaenqNYOX4-EPh5fb-Qc%3A49!18e15!24m112!1m30!13m9!2b1!3b1!4b1!6i1!8b1!9b1!14b1!20b1!25b1!18m19!3b1!4b1!5b1!6b1!9b1!13b1!14b1!17b1!20b1!21b1!22b1!27m1!1b0!28b0!32b1!33m1!1b1!34b1!36e2!10m1!8e3!11m1!3e1!14m1!3b0!17b1!20m2!1e3!1e6!24b1!25b1!26b1!27b1!29b1!30m1!2b1!36b1!37b1!39m3!2m2!2i1!3i1!43b1!52b1!54m1!1b1!55b1!56m1!1b1!61m2!1m1!1e1!65m5!3m4!1m3!1m2!1i224!2i298!72m22!1m8!2b1!5b1!7b1!12m4!1b1!2b1!4m1!1e1!4b1!8m10!1m6!4m1!1e1!4m1!1e3!4m1!1e4!3sother_user_google_review_posts__and__hotel_and_vr_partner_review_posts!6m1!1e1!9b1!89b1!90m2!1m1!1e2!98m3!1b1!2b1!3b1!103b1!113b1!114m3!1b1!2m1!1b1!117b1!122m1!1b1!126b1!127b1!26m4!2m3!1i80!2i92!4i8!30m28!1m6!1m2!1i0!2i0!2m2!1i530!2i735!1m6!1m2!1i820!2i0!2m2!1i870!2i735!1m6!1m2!1i0!2i0!2m2!1i870!2i20!1m6!1m2!1i0!2i715!2m2!1i870!2i735!34m19!2b1!3b1!4b1!6b1!8m6!1b1!3b1!4b1!5b1!6b1!7b1!9b1!12b1!14b1!20b1!23b1!25b1!26b1!31b1!37m1!1e81!42b1!47m0!49m10!3b1!6m2!1b1!2b1!7m2!1e3!2b1!8b1!9b1!10e2!50m4!2e2!3m2!1b1!3b1!67m5!7b1!10b1!14b1!15m1!1b0!69i769!77b1&q=Iraitech%20Innovations%20%26%20Technologies%20Pvt.Ltd&oq=Iraitech%20Innovations%20%26%20Technologies%20Pvt.Ltd&gs_l=maps.12..38i72k1l5.28180.28180.1.32180.1.1.....264.264.2-1.1.....0....1..maps..0.1.271.0..38i39k1j38.&tch=1&ech=1"
 
@@ -35,24 +35,17 @@ def build_url(company_name: str) -> str:
 
 
 def run_curl(url: str) -> str:
-    cmd = ["curl", "--location", url]
+    headers = {}
     for header in HEADERS:
-        cmd.extend(["--header", header])
-    cmd.extend(["--header", f"Cookie: {COOKIE}"])
+        if ": " in header:
+            key, value = header.split(": ", 1)
+            headers[key] = value
+    headers["Cookie"] = COOKIE
 
-    result = subprocess.run(cmd, capture_output=True, text=False)
-    if result.returncode != 0:
-        err_text = (result.stderr or b"").decode("utf-8", errors="replace").strip()
-        raise RuntimeError(err_text or "curl failed")
-
-    raw = result.stdout or b""
-    if not raw:
-        return ""
-
-    try:
-        return raw.decode("utf-8")
-    except UnicodeDecodeError:
-        return raw.decode("utf-8", errors="replace")
+    req = Request(url=url, headers=headers, method="GET")
+    with urlopen(req, timeout=30) as resp:
+        raw = resp.read() or b""
+    return raw.decode("utf-8", errors="replace")
 
 
 def extract_phones(text: str):
